27be91607ba0a06ec74b21b5391838df08e60d0f
Mikhail <miklmailplus@gmail.com>
2021-07-24 10:32:09 -1000

    completed quote-machine

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Colors_Array from './colorsArray';
+import quotesArray from './quotesDB';
+
 
 function App() {
+  const [quote, setQuote] = useState('Yesterday is history. Tomorrow is a mystery. Today is a gift.');
+  const [author, setAuthor] = useState('Eleanor Roosevelt');
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [BgrTxtColor, setBgrTxtColor] = useState("#E6B333");
+
+  // const Colors_Array = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+  // '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+  // '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A'];
+
+  // const quotesArray = [
+  //   {
+  //   quote: "Yesterday is history. Tomorrow is a mystery. Today is a gift.",
+  //   author: "Eleanor Roosevelt"}, 
+  //   {
+  //     quote:"Life isn’t about getting and having, it’s about giving and being.",
+  //   author:"Kevin Kruse"},
+  // {
+  //     quote:"Whatever the mind of man can conceive and believe, it can achieve.",
+  //     author:"Napoleon Hill"},
+  // {
+  //     quote:"Strive not to be a success, but rather to be of value.",
+  //     author:"Albert Einstein"},
+  // {
+  //     quote:"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+  //     author:"Robert Frost"},
+  // {
+  //     quote:"I attribute my success to this: I never gave or took any excuse.",
+  //     author:"Florence Nightingale"},
+  // {
+  //     quote:"You miss 100% of the shots you don’t take.", 
+  //     author:"Wayne Gretzky"},
+  // {
+  //     quote:"The best time to plant a tree was 20 years ago. The second best time is now.", 
+  //     author:"Chinese Proverb"},
+  // {
+  //     quote:"The most difficult thing is the decision to act, the rest is merely tenacity.", 
+  //     author:"Amelia Earhart"},
+  // {
+  //     quote:"Every strike brings me closer to the next home run.", 
+  //     author:"Babe Ruth"},
+  // {
+  //     quote:"Definiteness of purpose is the starting point of all achievement.", 
+  //     author:"W. Clement Stone"},
+  // {
+  //     quote:"We must balance conspicuous consumption with conscious capitalism.", 
+  //     author:"Kevin Kruse"},
+  // {
+  //     quote:"Life is what happens to you while you’re busy making other plans.", 
+  //     author:"John Lennon"},
+  // {
+  //     quote:"We become what we think about.", 
+  //     author:"Earl Nightingale"},
+  // {
+  //     quote:"An unexamined life is not worth living.", 
+  //     author: "Socrates"},
+  // {
+  //     quote:"Eighty percent of success is showing up.", 
+  //     author:"Woody Allen"},
+  // {
+  //     quote:"Your time is limited, so don’t waste it living someone else’s life.", 
+  //     author:"Steve Jobs"},
+  
+  // ]
+
+  const getRandomQuote = () => {
+    let randomInteger = Math.floor(Math.random() * quotesArray.length);
+    setRandomNumber(randomInteger);
+    setQuote(quotesArray[randomInteger].quote);
+    setAuthor(quotesArray[randomInteger].author);
+    setBgrTxtColor(Colors_Array[randomInteger]);
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{backgroundColor: BgrTxtColor, color: BgrTxtColor}}>
+        <div id="quote-box" style={{color: BgrTxtColor}}>
+          <p id='text'>
+          <i class="fa fa-quote-left" style={{color: BgrTxtColor}}></i>{quote}"
+          </p>
+          <p id='author'>
+          - {author}
+          </p>
+          <div className='button'>
+            <a id='tweet-quote' href="http://www.twitter.com/intent/tweet" style={{backgroundColor: BgrTxtColor}}>
+            <i class="fa fa-twitter" style={{backgroundColor: BgrTxtColor}}></i>
+            </a>
+          </div>          
+          <button id='new-quote' style={{backgroundColor: BgrTxtColor}} onClick={() => getRandomQuote()}>Get a random quote</button>
+          
+        </div>
       </header>
     </div>
   );
---
+++ src/quotesDB.js
@@ -0,0 +1,56 @@
+const quotesArray = [
+  {
+  quote: "Yesterday is history. Tomorrow is a mystery. Today is a gift.",
+  author: "Eleanor Roosevelt"}, 
+  {
+    quote:"Life isn’t about getting and having, it’s about giving and being.",
+  author:"Kevin Kruse"},
+{
+    quote:"Whatever the mind of man can conceive and believe, it can achieve.",
+    author:"Napoleon Hill"},
+{
+    quote:"Strive not to be a success, but rather to be of value.",
+    author:"Albert Einstein"},
+{
+    quote:"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    author:"Robert Frost"},
+{
+    quote:"I attribute my success to this: I never gave or took any excuse.",
+    author:"Florence Nightingale"},
+{
+    quote:"You miss 100% of the shots you don’t take.", 
+    author:"Wayne Gretzky"},
+{
+    quote:"The best time to plant a tree was 20 years ago. The second best time is now.", 
+    author:"Chinese Proverb"},
+{
+    quote:"The most difficult thing is the decision to act, the rest is merely tenacity.", 
+    author:"Amelia Earhart"},
+{
+    quote:"Every strike brings me closer to the next home run.", 
+    author:"Babe Ruth"},
+{
+    quote:"Definiteness of purpose is the starting point of all achievement.", 
+    author:"W. Clement Stone"},
+{
+    quote:"We must balance conspicuous consumption with conscious capitalism.", 
+    author:"Kevin Kruse"},
+{
+    quote:"Life is what happens to you while you’re busy making other plans.", 
+    author:"John Lennon"},
+{
+    quote:"We become what we think about.", 
+    author:"Earl Nightingale"},
+{
+    quote:"An unexamined life is not worth living.", 
+    author: "Socrates"},
+{
+    quote:"Eighty percent of success is showing up.", 
+    author:"Woody Allen"},
+{
+    quote:"Your time is limited, so don’t waste it living someone else’s life.", 
+    author:"Steve Jobs"},
+
+]
+
+export default quotesArray;
